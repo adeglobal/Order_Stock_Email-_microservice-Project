130667d2c8d1db0d2733d52bf754ad8b1d6adfc0
adeglobal <adeglobal2013@gmail.com>
2022-12-31 11:23:21 +0100

    implementing orderProducer

--- order-service/src/main/java/net/adeglobal/orderservice/config/KafkaTopicConfig.java
+++ order-service/src/main/java/net/adeglobal/orderservice/config/KafkaTopicConfig.java
@@ -2,7 +2,6 @@ package net.adeglobal.orderservice.config;
 
 
 import org.apache.kafka.clients.admin.NewTopic;
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
